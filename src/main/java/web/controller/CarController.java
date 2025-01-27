bbefa2c73de3a453bc439730c4b071a21002fd41
ValeriiaBubnova <paulo.insains@gmail.com>
2025-01-27 17:01:26 +0300

    добавила лог, подключила slf4j

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-
+@Slf4j
 @Controller
 public class CarController {
     private final CarService carService;
@@ -19,6 +20,7 @@ public class CarController {
     @GetMapping("/cars")
     public String getListOfCars(@RequestParam(defaultValue = "5") int count, Model model) {
         model.addAttribute("cars", carService.getCars(count));
+        log.info("Получено {} машин", count);
         return "cars";
     }
 
